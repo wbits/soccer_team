9a8aa5c821573fdc19916a93d0fb9f7a3877c662
Dick <dick@insided.nl>
2016-10-10 22:15:27 +0200

    created aggregate-root for player

---
+++ src/EventSourcingRepository/PlayerRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wbits\SoccerTeam\EventSourcingRepository;
+
+use Broadway\EventHandling\EventBusInterface;
+use Broadway\EventSourcing\AggregateFactory\PublicConstructorAggregateFactory;
+use Broadway\EventSourcing\EventSourcingRepository;
+use Broadway\EventStore\EventStoreInterface;
+
+class PlayerRepository extends EventSourcingRepository
+{
+    public function __construct(EventStoreInterface $eventStore, EventBusInterface $eventBus)
+    {
+        parent::__construct($eventStore, $eventBus, 'Player', new PublicConstructorAggregateFactory());
+    }
+}
--- src/Role/Player.php
+++ src/Role/Player.php
@@ -2,11 +2,14 @@
 
 namespace Wbits\SoccerTeam\Role;
 
+use Broadway\EventSourcing\EventSourcedAggregateRoot;
 use Doctrine\Common\Collections\ArrayCollection;
+use Wbits\SoccerTeam\Event\PlayerJoinsTheTeamEvent;
+use Wbits\SoccerTeam\Profile\Name;
 use Wbits\SoccerTeam\Profile\NameTrait;
 use Wbits\SoccerTeam\User\UserTrait;
 
-class Player implements RoleInterface
+class Player extends EventSourcedAggregateRoot implements RoleInterface
 {
     use NameTrait;
     use UserTrait;
@@ -16,9 +19,38 @@ class Player implements RoleInterface
      */
     private $parents;
 
+    /**
+     * @var string
+     */
+    private $playerId;
+
     public function __construct()
     {
         $this->parents = new ArrayCollection();
+    }
+
+    /**
+     * @param string $playerId
+     * @param string $firstName
+     * @param string $lastName
+     *
+     * @return Player
+     */
+    public static function joinsTheTeam(string $playerId, string $firstName, string $lastName): Player
+    {
+        $player = new Player();
+
+        $player->apply(new PlayerJoinsTheTeamEvent($playerId, $firstName, $lastName));
+
+        return $player;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAggregateRootId(): string
+    {
+        return $this->playerId;
     }
 
     /**
