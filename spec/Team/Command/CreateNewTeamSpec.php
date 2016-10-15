fa4bc60568122f1820cb114deac69552473084db
Dick <dick@insided.nl>
2016-10-15 21:52:38 +0200

    added create team command

--- spec/Team/Command/CreateNewTeamSpec.php
+++ spec/Team/Command/CreateNewTeamSpec.php
@@ -5,6 +5,7 @@ namespace spec\Wbits\SoccerTeam\Team\Command;
 use Wbits\SoccerTeam\Team\Command\CreateNewTeam;
 use PhpSpec\ObjectBehavior;
 use Wbits\SoccerTeam\Team\TeamId;
+use Wbits\SoccerTeam\Team\TeamInformation;
 
 class CreateNewTeamSpec extends ObjectBehavior
 {
@@ -13,14 +14,19 @@ class CreateNewTeamSpec extends ObjectBehavior
         $this->shouldHaveType(CreateNewTeam::class);
     }
 
-    function let(TeamId $teamId)
+    function let(TeamId $teamId, TeamInformation $information)
     {
-        $this->beConstructedWith($teamId);
+        $this->beConstructedWith($teamId, $information);
     }
 
     function it_should_have_a_team_id()
     {
         $result = $this->getTeamId();
         $result->shouldBeAnInstanceOf(TeamId::class);
+    }
+
+    function it_should_have_team_information()
+    {
+        $this->getInformation()->shouldReturnAnInstanceOf(TeamInformation::class);
     }
 }
